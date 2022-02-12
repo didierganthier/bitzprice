215efa465104e22b4c1d7f59b9d2f0fd5812c124
Woodens <woodgregory15@gmail.com>
2022-02-12 15:40:20 -0500

    time updated retrieved from api

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Layout from "../components/layout";
 
 
-const Index = () => (
+const Index = (props) => (
   <Layout>
     <div>
       <h1 className="text-3xl font-bold text-blue-500">Welcome to BitzPrice</h1>
+      {props.bpi.time.updated}
     </div>
   </Layout>  
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+
+  return {
+    bpi: data
+  }
+};
 
 export default Index;
